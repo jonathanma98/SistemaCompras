6fcca30f1e156149c79b611c5d80becf5827dd4c
Jonathan <504210987@ina.cr>
2021-07-08 13:05:55 -0600

    Intento de arreglo repositorio 02

---
+++ CapaPresentacion/frmInicio.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+using CapaNegocios;
+using CapaEntidades;
+using CapaUtilidades;
+
+namespace CapaPresentacion
+{
+    public partial class frmInicio : Form
+    {
+
+        private string contra, id;
+        int cont = 0;
+
+        List<tbAdmin> listaAdmin;
+
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            cont++;
+
+            if(cont == 100)
+            {
+                if(labelContra.Visible == true)
+                {
+                    labelContra.Visible = false;
+                }
+                if(labelId.Visible == true)
+                {
+                    labelId.Visible = false;
+                }
+                cont = 0;
+                timer1.Stop();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            cont++;
+
+            if (cont == 100)
+            {
+                timer2.Stop();
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void NewAdmin(object sender, EventArgs e)
+        {
+            frmRegistreAdmin registro = new frmRegistreAdmin();
+
+            registro.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            csNegocioLogin login = new csNegocioLogin();
+
+            id = txtId.Text;
+
+            listaAdmin = login.obtenerListaId(id);//enviamos el usuario id
+
+            if (listaAdmin.Count != 0){//evaluamos que el usario no venga vacio
+                foreach (tbAdmin dato in listaAdmin)
+                {
+                    contra = dato.contraseña.Trim();
+                }
+
+                string passport = csEncryp.GetSHA256(txtContra.Text);
+                //evaluamos los datos para poder ingresar a los formulacios
+                if (id == txtId.Text && passport == contra)
+                {
+                    frmWelcome frm = new frmWelcome();
+                    frm.ShowDialog();
+                    frmInicio inicio = new frmInicio();
+
+                    this.Opacity = 0;
+                    timer2.Start();
+
+                }
+                else if (contra != txtContra.Text)
+                {
+                    labelContra.Visible = true;
+                    timer1.Start();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Algo salio mal ");
+                labelId.Visible = true;
+                timer1.Start();
+            }
+
+        }
+    }
+}
---
+++ CapaPresentacion/frmRegistreAdmin.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using CapaEntidades;
+using CapaUtilidades;
+using CapaNegocios;
+
+namespace CapaPresentacion
+{
+    public partial class frmRegistreAdmin : Form
+    {
+        csNegocioLogin login = new csNegocioLogin();
+        public frmRegistreAdmin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if (verificarDatos())
+            {
+                tbAdmin admin = new tbAdmin();
+
+                admin.id = txtId.Text;
+                admin.Nombre = txtNombre.Text;
+                admin.contraseña = csEncryp.GetSHA256(txtContreseña.Text);
+                //MessageBox.Show(csEncryp.GetSHA256(txtContreseña.Text));
+                admin.telefono = txtTelefono.Text;
+                admin.correo = txtCorreo.Text;
+
+                if (login.guarda(admin))
+                {
+                    MessageBox.Show("Se ingrese un nuevo usuario", "Registro", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    //Close();
+                }
+                else
+                {
+                    MessageBox.Show("Algo selio mal al ingresa el usuario", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private bool verificarDatos()
+        {
+           if(txtId.Text == string.Empty)
+            {
+                MessageBox.Show("Ingrese una id", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtId.Focus();
+                return false;
+            }
+            if (txtNombre.Text == string.Empty)
+            {
+                MessageBox.Show("Ingrese un nombre", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtNombre.Focus();
+                return false;
+            }
+            if (txtContreseña.Text == string.Empty)
+            {
+                MessageBox.Show("Ingrese una contraseña", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                txtContreseña.Focus();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
